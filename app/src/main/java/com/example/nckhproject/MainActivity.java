47eeb7ffd245c4e390ec8214259783f5316bda61
enoughtodie99 <51414543+enoughtodie99@users.noreply.github.com>
2020-06-30 16:41:34 +0700

    Create project

---
+++ app/src/main/java/com/example/nckhproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nckhproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
